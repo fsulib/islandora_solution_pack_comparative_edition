ebe956eb62d8a56e1e51605de0593be37e2ba770
Bryan J. Brown <bjbrown@fsu.edu>
2017-01-13 20:44:07 +0000

    Pages get created via witness ingest, and all objects are routed through templates

---
+++ theme/islandora-genetic-edition-witness.tpl.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for genetic edition objects
+ */
+?>
+
+<h1>THIS IS A GENETIC EDITION WITNESS OBJECT!!1</h1>
---
+++ theme/islandora-genetic-edition.tpl.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for genetic edition objects
+ */
+?>
+
+<h1>THIS IS GENETIC EDITION OBJECT <?php print $islandora_object->id; ?>!!1</h1>
