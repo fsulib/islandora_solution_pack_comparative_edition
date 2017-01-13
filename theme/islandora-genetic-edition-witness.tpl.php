5a5e879f0ac8a10000afcf42108c20c1943113d2
Bryan J. Brown <bjbrown@fsu.edu>
2017-01-13 22:11:02 +0000

    Adds SPARQL query to find children in gened and witness templates

--- theme/islandora-genetic-edition-witness.tpl.php
+++ theme/islandora-genetic-edition-witness.tpl.php
@@ -4,6 +4,32 @@
  * @file
  * This is the template file for genetic edition objects
  */
+
+// Get child witness objects
+$query = <<<EOQ
+SELECT ?child FROM <#ri> WHERE {
+  ?child <http://islandora.ca/ontology/relsext#isMemberOf> <info:fedora/{$islandora_object->id}>
+}
+EOQ;
+$connection = islandora_get_tuque_connection();
+$results = $connection->repository->ri->sparqlQuery($query);
+$pids = array();
+foreach ($results as $result) {
+  $result = $result['child']['value'];
+  $pids[] = $result;
+}
+
 ?>
 
-<h1>THIS IS A GENETIC EDITION WITNESS OBJECT!!1</h1>
+<h1>I AM GENETIC EDITION WITNESS OBJECT <?php print $islandora_object->id; ?>!!1</h1>
+
+<h2>AND I HAVE THE FOLLOWING CHILDREN:</h2>
+<ul>
+
+<?php foreach ($pids as $pid) { ?>
+  <li><a href="/islandora/object/<?php print $pid?>"><?php print $pid; ?></a></li>
+<?php } ?>
+
+</ul>
+
+
--- theme/islandora-genetic-edition.tpl.php
+++ theme/islandora-genetic-edition.tpl.php
@@ -4,6 +4,30 @@
  * @file
  * This is the template file for genetic edition objects
  */
+
+// Get child witness objects
+$query = <<<EOQ
+SELECT ?child FROM <#ri> WHERE {
+  ?child <http://islandora.ca/ontology/relsext#isMemberOf> <info:fedora/{$islandora_object->id}>
+}
+EOQ;
+$connection = islandora_get_tuque_connection();
+$results = $connection->repository->ri->sparqlQuery($query);
+$pids = array();
+foreach ($results as $result) {
+  $result = $result['child']['value'];
+  $pids[] = $result;
+}
+
 ?>
 
-<h1>THIS IS GENETIC EDITION OBJECT <?php print $islandora_object->id; ?>!!1</h1>
+<h1>I AM GENETIC EDITION OBJECT <?php print $islandora_object->id; ?>!!1</h1>
+
+<h2>AND I HAVE THE FOLLOWING CHILDREN:</h2>
+<ul>
+
+<?php foreach ($pids as $pid) { ?>
+  <li><a href="/islandora/object/<?php print $pid?>"><?php print $pid; ?></a></li>
+<?php } ?>
+
+</ul>
