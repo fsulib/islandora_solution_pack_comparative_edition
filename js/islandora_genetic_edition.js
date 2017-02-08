035200d511016f002cc231a840efe22b1ca7f82b
Favenzio Calvo <fcalvo@fsu.edu>
2017-02-08 23:08:17 +0000

    Added comparison view to page display.

--- js/islandora_genetic_edition.js
+++ js/islandora_genetic_edition.js
@@ -2,5 +2,38 @@
 
 
 jQuery(document).ready(function($) {
+  
   $("#igesp-tabs").tabs();
+  $("#igesp-tabs-comparison").tabs();
+  
+  $("#igesp-focus-button").click(function() {
+    
+    //disable focus button and enable comparison button
+    $("#igesp-focus-button").prop('disabled', true);
+    $("#igesp-comparison-button").removeClass('igesp-button-active');
+    $("#igesp-focus-button").addClass('igesp-button-active');
+    $("#igesp-comparison-button").removeAttr('disabled');
+    
+    //hide comparison view
+    $("#igesp-tabs-comparison").css("display", "none");
+    $("#igesp-tabs").removeClass('igesp-left-column');
+    $("#igesp-tabs-comparison").removeClass('igesp-right-column');
+    
+  });
+  
+  $("#igesp-comparison-button").click(function() {
+    
+    //disable comparison button and enable focus button
+    $("#igesp-comparison-button").prop('disabled', true);
+    $("#igesp-focus-button").removeClass('igesp-button-active');
+    $("#igesp-comparison-button").addClass('igesp-button-active');
+    $("#igesp-focus-button").removeAttr('disabled');
+    
+    //show comparison view
+    $("#igesp-tabs-comparison").css("display", "block");
+    $("#igesp-tabs").addClass('igesp-left-column');
+    $("#igesp-tabs-comparison").addClass('igesp-right-column');
+
+  });
+  
 });
--- theme/islandora-genetic-edition-page.tpl.php
+++ theme/islandora-genetic-edition-page.tpl.php
@@ -8,10 +8,50 @@
 
 <div id="igesp-page-header">
   
-  <!--- TODO: ADD CONTROLS TO SWITCH PAGES -->
+  <div id="switch-view-controls">
+    <button type="button" id="igesp-focus-button" class="igesp-button-active" disabled>Focus View</button>
+    <button type="button" id="igesp-comparison-button">Comparison View</button>
+  </div>
   
 </div>  
   
-<div id="igesp-focus-tabs-container">
-  <?php print $page_image; ?>
+<div id="igesp-comparison-tabs-container">
+  
+  <div id="igesp-tabs">
+    <ul>
+      <li><a href="#tab-1">Image</a></li>
+      <li><a href="#tab-2">Transcript</a></li>
+      <li><a href="#tab-3">TEI</a></li>
+    </ul>
+  
+    <div id="tab-1">
+      <?php print $page_image; ?>
+    </div>
+    
+    <div id="tab-2">
+      <?php print $display_html; ?>
+    </div>
+    
+    <div id="tab-3">
+      <?php print $page_tei; ?>
+    </div>
+  
+  </div>
+  
+  <div id="igesp-tabs-comparison">
+    <ul>
+      <li><a href="#tab-comp-2">Transcript</a></li>
+      <li><a href="#tab-comp-3">TEI</a></li>
+    </ul>
+    
+    <div id="tab-comp-2">
+      <?php print $display_html; ?>
+    </div>
+    
+    <div id="tab-comp-3">
+      <?php print $page_tei; ?>
+    </div>
+  
+  </div>
+  
 </div>
